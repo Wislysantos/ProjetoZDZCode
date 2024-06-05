b0631e1407471f400d45641ffe637fd035f46233
Wislysantos <wislysantos@gmail.com>
2024-06-05 19:16:42 -0300

    feat: add coluna de preco dos produtos na modal e valor da compra

--- ProjetoZDZCodeFrontEnd/pages/historicodevenda.vue
+++ ProjetoZDZCodeFrontEnd/pages/historicodevenda.vue
@@ -3,19 +3,34 @@
     <v-card>
       <v-row class="align-center">
         <v-col>
-          <v-card-title  class="mr-4">
+          <v-card-title class="mr-4">
             Historico de Venda
           </v-card-title>
-        </v-col>        
+        </v-col>
       </v-row>
 
-      <v-dialog v-model="dialog" max-width="500px">
+      <v-dialog v-model="dialog" max-width="600px">
         <v-card>
           <v-card-title>
-            <span class="headline">Produtos vendido</span>
+            <span class="headline">Produtos vendidos</span>
           </v-card-title>
           <v-card-text>
-            
+            <v-data-table
+              :headers="productHeaders"
+              :items="venda.produtos"
+              class="elevation-1"
+              dark
+            >
+              <template v-slot:item.total="{ item }">
+                <span>{{ (item.preco * item.quantidade).toFixed(2) }}</span>
+              </template>
+            </v-data-table>
+            <v-divider class="my-4"></v-divider>
+            <v-row>
+              <v-col class="text-right">
+                <v-subheader>Total dos Produtos: R$ {{ calcularTotalProdutos(venda.produtos) }}</v-subheader>
+              </v-col>
+            </v-row>
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
@@ -62,10 +77,10 @@ export default {
   name: 'IndexPage',
   data () {
     const listProdutos = [
-      { name: 'Ice cream sandwich', calories: 237, price: 5, available: 10 },
-      { name: 'Eclair', calories: 262, price: 7, available: 5 },
-      { name: 'Cupcake', calories: 305, price: 4, available: 8 },
-      { name: 'Gingerbread', calories: 356, price: 6, available: 12 },
+      { nome: 'Ice cream sandwich', caloreias: 237, preco: 5.92, quantidade: 10 },
+      { nome: 'Eclair', caloreias: 262, preco: 7, quantidade: 5 },
+      { nome: 'Cupcake', caloreias: 305, preco: 4, quantidade: 8 },
+      { nome: 'Gingerbread', caloreias: 356, preco: 6, quantidade: 12 },
     ]
     const vendas = [
       { codigDaVenda: '6152', valorTotal: 213.00, dataDaVenda: "05/07/2024", produtos: listProdutos},
@@ -74,25 +89,15 @@ export default {
       { codigDaVenda: '3433', valorTotal: 877.99, dataDaVenda: "05/07/2024", produtos: listProdutos},
       { codigDaVenda: '3234', valorTotal: 455.01, dataDaVenda: "05/07/2024", produtos: listProdutos},
       { codigDaVenda: '4322', valorTotal: 842.05, dataDaVenda: "05/07/2024", produtos: listProdutos},
-    
     ];
-
-    
 
     return {
       dialog: false,
-      isEditing: false,
-      vendas:{
+      venda: {
         codigDaVenda: '',
         valorTotal: 0,
         dataDaVenda: '',
         produtos: []
-      },
-      listProdutos: {
-        codigDaVenda: '',
-        calorias: '',
-        preco: 0,
-        quantidade: 0
       },
       search: '',
       headers: [
@@ -101,17 +106,24 @@ export default {
         { text: 'Data da venda', value: 'dataDaVenda' },
         { text: 'Actions', value: 'actions', sortable: false },
       ],
+      productHeaders: [
+        { text: 'Nome', value: 'nome' },
+        { text: 'Calorias', value: 'caloreias' },
+        { text: 'Preço', value: 'preco' },
+        { text: 'Quantidade', value: 'quantidade' },
+        { text: 'Total', value: 'total' },
+      ],
       desserts: vendas,
     }
   },
   methods: {
     abrirProdutosDavenda(item) {
-      this.isEditing = true;
       this.venda = { ...item };
       this.dialog = true;
     },
-   
-   
+    calcularTotalProdutos(produtos) {
+      return produtos.reduce((total, produto) => total + (produto.preco * produto.quantidade), 0).toFixed(2);
+    }
   }
 }
 </script>
